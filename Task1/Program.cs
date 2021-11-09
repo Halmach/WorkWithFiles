a007a7a79d3ef41b822afb924299dcc349c78c34
Khalmatov <large000@yandex.ru>
2021-11-09 22:26:09 +0300

    добавил проверки вложенных файлов в цикле до удаления директории

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -71,7 +71,7 @@ namespace Task1
             }
 
             // без этого цикла программа не успевает удалять каталоги большой вложенности и появляется исключение,поэтому перед удалением следует проверить количество вложенных каталогов
-            while (drInfo.GetDirectories().Length != 0);
+            while (drInfo.GetDirectories().Length != 0 || drInfo.GetFiles().Length != 0);
             drInfo.Delete();
 
         }
