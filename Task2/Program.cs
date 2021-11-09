034997d1963aeaf06d58a87bf34e34054d5cee3a
Khalmatov <large000@yandex.ru>
2021-11-09 23:00:01 +0300

    Task2 вычисляет по указанному пути размер каталога в байтах с учетом всех вложенных файлов и папок

---
+++ Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\testDeleteFiles";
+            long totalSize = 0;
+            Console.WriteLine("Введите путь до каталога:");
+            path = Console.ReadLine();
+            try
+            {
+                if (path.Trim() != "")
+                {
+                    DirectoryInfo dr = new DirectoryInfo(path);
+                    if (dr.Exists)
+                    {
+                        Console.WriteLine("Указанный каталог существует");
+                        DirectoryInfo[] dirs = dr.GetDirectories();
+                        FileInfo[] files = dr.GetFiles();
+
+                        foreach (FileInfo file in files)
+                        {
+                            totalSize += file.Length;
+                        }
+
+                        foreach (DirectoryInfo dir in dirs)
+                        {
+                            totalSize += CalculateSizeAllFilesAndDirectories(dir);
+                        }
+
+                        Console.WriteLine($"Размер указанного каталога составляет: {totalSize}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Указанного каталога не существует");
+                    }
+                }
+                else Console.WriteLine("Не указан путь");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+        internal static long CalculateSizeAllFilesAndDirectories(DirectoryInfo drInfo)
+        {
+            DirectoryInfo[] dirs = drInfo.GetDirectories();
+            FileInfo[] files = drInfo.GetFiles();
+            long totalSize = 0;
+            foreach (FileInfo file in files)
+            {
+                totalSize += file.Length;
+            }
+            foreach (DirectoryInfo dir in dirs)
+            {
+                totalSize += CalculateSizeAllFilesAndDirectories(dir);
+            }
+
+            return totalSize;
+
+        }
+    }
+
+}
