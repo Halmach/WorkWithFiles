8b8d2d11abad2e3286fec1703dbdb334621fbd75
Khalmatov <large000@yandex.ru>
2021-11-09 22:23:07 +0300

    добавил комментарий по поводу проверки наличия вложенных каталогов

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -70,6 +70,7 @@ namespace Task1
                 DeleteAllFilesAndDirectories(dir);
             }
 
+            // без этого цикла программа не успевает удалять каталоги большой вложенности и появляется исключение,поэтому перед удалением следует проверить количество вложенных каталогов
             while (drInfo.GetDirectories().Length != 0);
             drInfo.Delete();
 
